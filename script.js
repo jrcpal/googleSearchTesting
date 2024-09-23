505b6053e3b49423b701d5319c76335c8580acb4
Jessica Churgin <jchurgy@gmail.com>
2024-09-23 14:46:35 -0400

    expand test

--- script.js
+++ script.js
@@ -8,9 +8,11 @@ const googleDatabase = [
     'myfavoritecats2.com'
 ];
 
-const googleSearch = (searchInput, db) => {
+function googleSearch (searchInput, db) {
     const matches = db.filter(website => website.includes(searchInput));
     return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
 
-console.log(googleSearch('cat', googleDatabase));
+console.log(googleSearch('cat', googleDatabase));      
+
+module.exports = googleSearch;
